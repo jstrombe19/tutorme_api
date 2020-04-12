fb652bdc66192c112577cecdcdfb24d53c767ad3
jstrombe19 <jaredstromberg@gmail.com>
2020-04-11 18:43:45 -0700

    Redefined the relationships for the user model. I suspect some of the issues of the original repo are the result of sloppy relationship definitions contained in the original repo's user.rb file.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :appointments, foreign_key: :student_id, dependent: :destroy
+  has_many :tutors, through: :appointments, class_name: "User"
+  has_many :student_appointments, foreign_key: :tutor_id, class_name: "Appointment", dependent: :destroy 
+  has_many :students, through: :student_appointments, class_name: "User"
+  has_many :tutor_subjects, foreign_key: :tutor_id, dependent: :destroy 
+  has_many :subjects, through: :tutor_subjects 
+  
+  validates :username, uniqueness: {message: "%{attribute}s must be unique and %{value} has already been taken."}
+
 end
