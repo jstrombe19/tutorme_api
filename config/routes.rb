a4a952d078371867fa6438ddcff7cfb4b0cdab6f
jstrombe19 <jaredstromberg@gmail.com>
2020-04-11 20:55:42 -0700

    Updated routes to only those that should exist.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :tutor_subjects
-  resources :subjects
-  resources :appointments
-  resources :users
+  resources :tutor_subjects, except: [:new, :edit]
+  resources :subjects, except: [:new, :edit]
+  resources :appointments, except: [:new, :edit]
+  resources :users, except: [:show, :new, :edit]
+
+  post "login", to: "authentication#login"
+  get "profile", to: "users#profile"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
